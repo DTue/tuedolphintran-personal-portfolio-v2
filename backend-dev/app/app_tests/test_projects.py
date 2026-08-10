c81b00fb54d1f0b72371d52d89643ef48b0e1754
Tue(Dolphin) Tran <tuetamtran2307@gmail.com>
2026-08-10 14:25:02 +0700

    FIXED: created_at for projects model

--- backend-dev/app/app_tests/test_projects.py
+++ backend-dev/app/app_tests/test_projects.py
@@ -3,9 +3,28 @@ from app.models.projects import Project
 from sqlalchemy import select
 
 
+
+#mock 
+project = Project(
+    title="DARK",
+    slug="dark",
+    category="Software",
+    role="Lead Developer",
+    summary="Privacy-first AI SMS system connecting unhoused individuals with community resources.",
+    image_url=None,
+    github_url=None,
+    demo_url=None,
+    featured=True,
+    display_order=1,
+    
+)
+
 my_session = get_session()
 
 try:
+    my_session.add(project) #add mock project data
+    my_session.commit() #commit changes
+    my_session.refresh(project) #reload values
     statement = select(Project) #accep new column args
     projects = my_session.scalars(statement).all()
 
--- backend-dev/app/models/projects.py
+++ backend-dev/app/models/projects.py
@@ -9,9 +9,10 @@ Responsibilities:
 
 """
 
-from sqlalchemy import Integer, Boolean, String, Text
+from sqlalchemy import Integer, Boolean, String, Text, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column
 from app.models.base import Base
+from datetime import datetime
 
 
 
@@ -32,7 +33,7 @@ class Project(Base):
     demo_url: Mapped[str | None] = mapped_column(Text, nullable=True)
     featured: Mapped[bool] = mapped_column(Boolean, nullable=True)
     display_order: Mapped[int] = mapped_column(Integer, nullable=False)
-    created_at: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now(), nullable=False)
 
     def __repr__(self) -> str:
         return f"Project(id={self.id}, title={self.title}, slug={self.slug}, category={self.category}, role={self.role})"
